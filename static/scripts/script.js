76dd63f299fb89742bf67ca2d05fbcb9018e90fe
FrostingBunBun <85452368+FrostingBunBun@users.noreply.github.com>
2023-05-15 20:26:34 +0300

    god it works, goodsave

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -76,62 +76,29 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
 
-  submitButton.addEventListener("click", function () {
-  var leftField = document.getElementById("field1");
-  var rightField = document.getElementById("field2");
-  var errorMessage = document.getElementById("error-message");
+  submitButton.addEventListener("click", function() {
+    var leftField = document.getElementById("field1");
+    var rightField = document.getElementById("field2");
+    var errorMessage = document.getElementById("error-message");
 
-  if (
-    (leftField.textContent.trim() !== "" && leftField.textContent.trim() !== "Player Left" && leftField.textContent.trim() !== "Clear") &&
-    (rightField.textContent.trim() !== "" && rightField.textContent.trim() !== "Player Right" && rightField.textContent.trim() !== "Clear")
-  ) {
-    var leftName = leftField.textContent;
-    var rightName = rightField.textContent;
+    if (
+      leftField.textContent.trim() !== "" &&
+      leftField.textContent.trim() !== "Player Left" &&
+      rightField.textContent.trim() !== "" &&
+      rightField.textContent.trim() !== "Player Right"
+    ) {
+    confirmationModal.style.display = "block";
+    }
+    else {
+      errorMessage.textContent = "Please enter names into both fields.";
+      errorMessage.classList.add("show");
 
-    // Create containers for the players
-    var leftContainer = document.createElement("div");
-    leftContainer.classList.add("player-container");
-    var rightContainer = document.createElement("div");
-    rightContainer.classList.add("player-container");
-
-    // Create elements for player information
-    var leftInfo = document.createElement("p");
-    leftInfo.textContent = "Player Left Info"; // Replace with actual information
-    var rightInfo = document.createElement("p");
-    rightInfo.textContent = "Player Right Info"; // Replace with actual information
-
-    // Append player information to the containers
-    leftContainer.appendChild(leftInfo);
-    rightContainer.appendChild(rightInfo);
-
-    // Create match result element
-    var matchResult = document.createElement("p");
-    matchResult.textContent = leftName + " VS " + rightName;
-
-    // Clear the field container
-    var fieldContainer = document.getElementById("field-container");
-    fieldContainer.innerHTML = "";
-
-    // Append the containers and match result to the field container
-    fieldContainer.appendChild(leftContainer);
-    fieldContainer.appendChild(matchResult);
-    fieldContainer.appendChild(rightContainer);
-
-    confirmationModal.style.display = "none";
-    cancelButton.style.display = "block";
-
-    // Reset error message
-    errorMessage.textContent = "";
-  } else {
-    errorMessage.textContent = "Please enter names into both fields.";
-    errorMessage.classList.add("show");
-
-    // Remove the error class after 2 seconds
-    setTimeout(function () {
-      errorMessage.classList.remove("show");
-    }, 2000);
-  }
-});
+      // Remove the error class after 2 seconds
+      setTimeout(function() {
+        errorMessage.classList.remove("show");
+      }, 2000);
+    }
+  });
 
   
   
@@ -161,57 +128,49 @@ document.addEventListener("DOMContentLoaded", function () {
     item.textContent = item.textContent.slice(2, -2);
   });
 
-  confirmYes.addEventListener("click", function () {
+  confirmYes.addEventListener("click", function() {
     var leftField = document.getElementById("field1");
     var rightField = document.getElementById("field2");
-  
+
     if (
-      (leftField.textContent.trim() !== "" && leftField.textContent.trim() !== "Player Left" && leftField.textContent.trim() !== "Clear") &&
-      (rightField.textContent.trim() !== "" && rightField.textContent.trim() !== "Player Right" && rightField.textContent.trim() !== "Clear")
+      leftField.textContent.trim() !== "" &&
+      leftField.textContent.trim() !== "Player Left" &&
+      rightField.textContent.trim() !== "" &&
+      rightField.textContent.trim() !== "Player Right"
     ) {
       var leftName = leftField.textContent;
       var rightName = rightField.textContent;
-  
+
       var matchResult = document.createElement("p");
       matchResult.textContent = leftName + " VS " + rightName;
       var fieldContainer = document.getElementById("field-container");
       if (fieldContainer) {
         fieldContainer.innerHTML = "";
         fieldContainer.appendChild(matchResult);
-  
-        confirmationModal.style.display = "none";
-  
-        // Show the cancel button
-        cancelButton.style.display = "block";
       }
-    } else {
-      // var errorMessage = document.getElementById("error-message");
-      // errorMessage.textContent = "Please enter names into both fields.";
-    }
-  });
-  
 
-  confirmNo.addEventListener("click", function () {
+      // Hide the cancel button
+      cancelButton.style.display = "block";
+    }
+
     confirmationModal.style.display = "none";
   });
   
-  cancelButton.addEventListener("click", function () {
+
+  confirmNo.addEventListener("click", function() {
+    confirmationModal.style.display = "none";
+  });
+  
+  cancelButton.addEventListener("click", function() {
     var leftField = document.getElementById("field1");
     var rightField = document.getElementById("field2");
-  
+
     leftField.textContent = "Player Left";
     rightField.textContent = "Player Right";
-  
+
     // Hide the cancel button
     cancelButton.style.display = "none";
-  
-    // Show the submit button
-    submitButton.style.display = "block";
-  
-    // Show the list
-    document.getElementById("list").classList.remove("list-hidden");
   });
-
 
 
 
