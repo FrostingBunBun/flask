5b1396ccddb7571d46799ed70e45b393ca106df8
FrostingBunBun <85452368+FrostingBunBun@users.noreply.github.com>
2023-05-20 08:49:01 +0300

    qwe

--- app.py
+++ app.py
@@ -2,11 +2,11 @@ from flask import Flask
 from views import views
 from flask_redis import FlaskRedis
 
-app = Flask(__name__)
-app.secret_key = "secretKey"  # Set a secret key for session management
+application = Flask(__name__)
+application.secret_key = "secretKey"  # Set a secret key for session management
 
 
-app.register_blueprint(views, url_prefix="/")
+application.register_blueprint(views, url_prefix="/")
 
 if __name__ == "__main__":
-    app.run(debug=True, port=8000)
+    application.run(host='0.0.0.0')
